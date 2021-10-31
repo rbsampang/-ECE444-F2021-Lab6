ffdf3d9491c666c8e19606913c3784ddb292a2f5
rbsampang <rbsam99@gmail.com>
2021-10-31 18:34:58 -0400

    add back the database URL fix

--- project/app.py
+++ project/app.py
@@ -25,7 +25,7 @@ PASSWORD = "admin"
 SECRET_KEY = "change_me"
 SQLALCHEMY_DATABASE_URI = os.getenv(
     "DATABASE_URL", f"sqlite:///{Path(basedir).joinpath(DATABASE)}"
-)
+).replace("://", "ql://", 1)
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
